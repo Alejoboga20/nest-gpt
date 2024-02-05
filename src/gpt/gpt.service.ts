86883d36a8cd7e04ab079e889bc9f137be8a8796
alejoboga-ai <alejo@zelta.ai>
2024-02-05 12:13:23 -0300

    implement new use case

--- src/gpt/gpt.service.ts
+++ src/gpt/gpt.service.ts
@@ -3,6 +3,8 @@ import OpenAI from 'openai';
 
 import { orthographyCheckUseCase } from './use-cases/orthography-check.use-case';
 import { OrthographyDto } from './dtos';
+import { ProsConsDiscusserDto } from './dtos/pros-cons-discusser.dto';
+import { prosConsDiscusserUseCase } from './use-cases/pros-cons-discusser.use-case';
 
 @Injectable()
 export class GptService {
@@ -13,6 +15,14 @@ export class GptService {
   async orthographyCheck(orthographyDto: OrthographyDto) {
     const result = await orthographyCheckUseCase(this.openai, {
       prompt: orthographyDto.prompt,
+    });
+
+    return result;
+  }
+
+  async prosConsDicusser({ prompt }: ProsConsDiscusserDto) {
+    const result = await prosConsDiscusserUseCase(this.openai, {
+      prompt,
     });
 
     return result;
---
+++ src/gpt/use-cases/pros-cons-discusser.use-case.ts
@@ -0,0 +1,30 @@
+import OpenAI from 'openai';
+
+interface Args {
+  prompt: string;
+  maxTokens?: number;
+}
+
+export const prosConsDiscusserUseCase = async (
+  openai: OpenAI,
+  { prompt, maxTokens }: Args,
+) => {
+  const completion = await openai.chat.completions.create({
+    messages: [
+      {
+        role: 'system',
+        content: `
+              You will be given a question and your task is to give an answer with pros and cons,
+              You must answer in markdown format, and pros and cons must be in a list.`,
+      },
+      { role: 'user', content: prompt },
+    ],
+    model: 'gpt-3.5-turbo',
+    temperature: 0.3,
+    max_tokens: maxTokens,
+  });
+
+  return {
+    completion: completion.choices[0].message,
+  };
+};
